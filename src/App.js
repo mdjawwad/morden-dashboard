49af2a5200789b26ef3899274f0f2cc09949ebab
mdjawwad <mdjawwad790@gmail.com>
2022-07-04 01:30:34 +0530

    Done..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Footer, Sidebar } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Employees,
+  Customers,
+} from "./pages";
+import { useStateContext } from "./contexts/ContextProvider";
+
+
 
 function App() {
+  const { activeMenu } = useStateContext()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 " style={{ zIndex: "1000" }}>
+            <TooltipComponent content="settings" position="Top">
+              <button
+                type="button"
+                className="text-2xl p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div
+              className="w-72 fixed sidebar dark:bg-secondary-dark-bg
+            bg-white "
+            >
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg main-h-screen  w-full ${activeMenu ? "md:ml-72" : "flex-2"
+              }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+            <div>
+              <Routes>
+                {/* Dashboard */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
+
+                {/* Pages */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as SparkLine } from './Charts/SparkLine';
+export { default as Stacked } from './Charts/Stacked';
+export { default as Header } from './Header';
